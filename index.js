40962ba6ddaab38dcc4216da7cb95a8a71cefe71
Lucca Rendall <luccarendall2@gmail.com>
2022-08-10 20:06:27 -0300

    Get Talker

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
-
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+const talkerInfo = './talker.json';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
-
 app.listen(PORT, () => {
   console.log('Online');
 });
+
+app.get('/talker', async (_req, res) => {
+  const talker = await fs.readFile(talkerInfo, 'utf-8');
+  const talkerJSON = JSON.parse(talker);
+  res.status(200).json(talkerJSON);
+}); 
